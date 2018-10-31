a6e8016a4c55efaeafbad3eb1a7df074813ce715
denizhan aydinli <daydinli93@gmail.com>
2018-11-01 00:37:10 +0300

    route structure initialized with simple demo

--- src/router/app_router.js
+++ src/router/app_router.js
@@ -2,38 +2,45 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import SimpleAppBar from "../components/app_bar";
+import { AlbumPage } from "../components/album";
+import { MyAppBar } from "../components/MyAppBar";
+import { AlbumList } from "../components/albums";
 
 
 const Index = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Users = () => <h2>Users</h2>;
-const Albums = () => <SimpleAppBar />;
-// const Albums = () => <h2>Users</h2>;
+
+const MyAccount = () => (
+    <div>
+        <MyAppBar>
+            <h2>My Account</h2>
+        </MyAppBar>
+    </div>
+);
+
+const AlbumPhotos = () => (
+    <div>
+        <MyAppBar>
+            <AlbumPage />
+        </MyAppBar>
+    </div>
+);
+
+const AlbumCollection = () => (
+    <div>
+        <MyAppBar>
+            <AlbumList />
+        </MyAppBar>
+    </div>
+);
+
 
 export const AppRouter = () => (
     <Router>
         <div>
-            {/* <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about/">About</Link>
-                    </li>
-                    <li>
-                        <Link to="/users/">Users</Link>
-                    </li>
-                    <li>
-                        <Link to="/albums/">Albums</Link>
-                    </li>
-                </ul>
-            </nav> */}
-
             <Route path="/" exact component={Index} />
-            <Route path="/about/" component={About} />
-            <Route path="/users/" component={Users} />
-            <Route path="/albums/" component={Albums} />
+            <Route path="/my_account/" component={MyAccount} />
+            <Route path="/album/1" component={AlbumPhotos} />
+            <Route path="/albums/" component={AlbumCollection} />
         </div>
     </Router>
 );
