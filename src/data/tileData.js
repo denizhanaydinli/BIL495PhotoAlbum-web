8f20ac5afadba5aa322e9e3e7fedc21ab3598459
denizhan aydinli <daydinli93@gmail.com>
2018-11-01 00:38:00 +0300

    images for demo

---
+++ src/data/tileData.js
@@ -0,0 +1,84 @@
+import bike from './static/bike.jpg';
+import breakfast from './static/breakfast.jpg';
+import burgers from './static/burgers.jpg';
+import camera from './static/camera.jpg';
+import hats from './static/hats.jpg';
+import honey from './static/honey.jpg';
+import morning from './static/morning.jpg';
+import mushroom from './static/mushroom.jpg';
+import olive from './static/olive.jpg';
+import plant from './static/plant.jpg';
+import star from './static/star.jpg';
+import vegetables from './static/vegetables.jpg';
+
+
+
+const tileData = [
+    {
+        img: breakfast,
+        title: 'Breakfast',
+        author: 'jill111',
+        cols: 2,
+        featured: true,
+    },
+    {
+        img: burgers,
+        title: 'Tasty burger',
+        author: 'director90',
+    },
+    {
+        img: camera,
+        title: 'Camera',
+        author: 'Danson67',
+    },
+    {
+        img: morning,
+        title: 'Morning',
+        author: 'fancycrave1',
+        featured: true,
+    },
+    {
+        img: hats,
+        title: 'Hats',
+        author: 'Hans',
+    },
+    {
+        img: honey,
+        title: 'Honey',
+        author: 'fancycravel',
+    },
+    {
+        img: vegetables,
+        title: 'Vegetables',
+        author: 'jill111',
+        cols: 2,
+    },
+    {
+        img: plant,
+        title: 'Water plant',
+        author: 'BkrmadtyaKarki',
+    },
+    {
+        img: mushroom,
+        title: 'Mushrooms',
+        author: 'PublicDomainPictures',
+    },
+    {
+        img: olive,
+        title: 'Olive oil',
+        author: 'congerdesign',
+    },
+    {
+        img: star,
+        title: 'Sea star',
+        cols: 2,
+        author: '821292',
+    },
+    {
+        img: bike,
+        title: 'Bike',
+        author: 'danfador',
+    },
+];
+
+export default tileData;
