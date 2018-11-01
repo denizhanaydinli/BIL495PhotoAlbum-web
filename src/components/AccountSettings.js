8bdbe112fc2ac2a58cdd6ce9bed34d781929499f
denizhan aydinli <daydinli93@gmail.com>
2018-11-01 17:23:08 +0300

    account settings initial page

---
+++ src/components/AccountSettings.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+
+const styles = theme => ({
+    button: {
+        display: 'block',
+        marginTop: theme.spacing.unit * 2,
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+});
+
+
+
+class AccountSettings extends Component {
+    state = {
+        age: '',
+        open: false,
+    };
+
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    handleOpen = () => {
+        this.setState({ open: true });
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <form autoComplete="off">
+                <Button className={classes.button} onClick={this.handleOpen}>
+                    Select Hour for deleting photos automatically
+            </Button>
+                <FormControl className={classes.formControl}>
+                    <InputLabel htmlFor="demo-controlled-open-select">Hour</InputLabel>
+                    <Select
+                        open={this.state.open}
+                        onClose={this.handleClose}
+                        onOpen={this.handleOpen}
+                        value={this.state.age}
+                        onChange={this.handleChange}
+                        inputProps={{
+                            name: 'age',
+                            id: 'demo-controlled-open-select',
+                        }}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>10</MenuItem>
+                        <MenuItem value={20}>20</MenuItem>
+                        <MenuItem value={30}>30</MenuItem>
+                    </Select>
+                </FormControl>
+            </form>
+        );
+    }
+}
+
+AccountSettings.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AccountSettings);
